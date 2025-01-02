3b7eca3512095d19f12a4c19e345a9ea11d0d083
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-01-02 14:20:17 -0500

    Update Menu

--- lib/controllers/exit.dart
+++ lib/controllers/exit.dart
@@ -5,16 +5,30 @@ Future<bool> showExitConfirmationDialog(BuildContext context) async {
   final shouldPop = await showDialog<bool>(
     context: context,
     builder: (context) => AlertDialog(
-      title: const Text('¿Estás seguro?'),
-      content: const Text('¿Quieres salir de la aplicación?'),
+      title: const Text(
+        '¿Estás seguro?',
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.blue),
+      ),
+      content: const Text(
+        '¿Quieres salir de la aplicación?',
+        textAlign: TextAlign.center,
+      ),
       actions: [
         TextButton(
           onPressed: () => Navigator.pop(context, false), // Retorna false
-          child: const Text('No'),
+          child: const Text(
+            'No',
+            style: TextStyle(color: Colors.blue),
+            textAlign: TextAlign.center,
+          ),
         ),
         TextButton(
           onPressed: () => SystemNavigator.pop(), // Cierra la app
-          child: const Text('Sí'),
+          child: const Text(
+            'Sí',
+            style: TextStyle(color: Colors.blue),
+          ),
         ),
       ],
     ),
