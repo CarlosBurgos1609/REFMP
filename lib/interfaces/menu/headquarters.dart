09204ed732db1abd33cd80eedf230006108c8232
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-02-13 21:24:13 -0500

    Update headquarters

--- lib/interfaces/menu/headquarters.dart
+++ lib/interfaces/menu/headquarters.dart
@@ -13,6 +13,12 @@ class HeadquartersPage extends StatefulWidget {
 }
 
 class _HeadquartersPageState extends State<HeadquartersPage> {
+  Future<List<dynamic>> _fetchData() async {
+    // Obtener los datos de Supabase
+    final response = await Supabase.instance.client.from("sedes").select();
+    return response;
+  }
+
   void _openMap(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
@@ -46,104 +52,116 @@ class _HeadquartersPageState extends State<HeadquartersPage> {
         drawer: Menu.buildDrawer(context),
         body: Builder(
           builder: (context) {
-            return FutureBuilder(
-              future: Supabase.instance.client.from("sedes").select(),
-              builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(
+            return RefreshIndicator(
+              onRefresh: () async {
+                // Forzar la actualización del FutureBuilder
+                setState(() {});
+              },
+              child: FutureBuilder(
+                future: _fetchData(),
+                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(
                       child: Text("No hay sedes disponibles",
-                          style: TextStyle(color: Colors.blue)));
-                }
+                          style: TextStyle(color: Colors.blue)),
+                    );
+                  }
 
-                // Ordenar alfabéticamente por el campo "name"
-                snapshot.data!.sort(
-                    (a, b) => (a["name"] ?? "").compareTo(b["name"] ?? ""));
+                  // Ordenar alfabéticamente por el campo "name"
+                  snapshot.data!.sort(
+                      (a, b) => (a["name"] ?? "").compareTo(b["name"] ?? ""));
 
-                return ListView(
-                  children: snapshot.data!.map((doc) {
-                    // Asegurar que todos los campos estén presentes
-                    final name = doc["name"] ?? "Nombre no disponible";
-                    final address = doc["address"] ?? "Dirección no disponible";
-                    final description = doc["description"] ?? "Sin descripción";
-                    final contactNumber =
-                        doc["contact_number"] ?? "No disponible";
-                    final ubication = doc["ubication"] ?? "";
-                    final photo =
-                        doc["photo"] ?? "https://via.placeholder.com/150";
+                  return ListView(
+                    children: snapshot.data!.map((doc) {
+                      // Asegurar que todos los campos estén presentes
+                      final name = doc["name"] ?? "Nombre no disponible";
+                      final address =
+                          doc["address"] ?? "Dirección no disponible";
+                      final description =
+                          doc["description"] ?? "Sin descripción";
+                      final contactNumber =
+                          doc["contact_number"] ?? "No disponible";
+                      final ubication = doc["ubication"] ?? "";
+                      final photo =
+                          doc["photo"] ?? "https://via.placeholder.com/150";
 
-                    return Card(
-                      margin: const EdgeInsets.all(10),
-                      elevation: 5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              photo,
-                              width: double.infinity,
-                              height: 200,
-                              fit: BoxFit.cover,
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                photo,
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  name,
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(description),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    const Icon(Icons.location_on,
-                                        color: Colors.red),
-                                    const SizedBox(width: 5),
-                                    Expanded(
-                                      child: GestureDetector(
-                                        onTap: () => _openMap(ubication),
-                                        child: Text(
-                                          address,
-                                          style: const TextStyle(
+                            Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    name,
+                                    style: const TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(description),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.location_on,
+                                          color: Colors.blue),
+                                      const SizedBox(width: 5),
+                                      Expanded(
+                                        child: GestureDetector(
+                                          onTap: () => _openMap(ubication),
+                                          child: Text(
+                                            address,
+                                            style: const TextStyle(
                                               color: Colors.blue,
-                                              decoration:
-                                                  TextDecoration.underline),
+                                              // decoration:
+                                              //     TextDecoration.underline,
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  children: [
-                                    const Icon(Icons.phone,
-                                        color: Colors.green),
-                                    const SizedBox(width: 5),
-                                    Text(contactNumber),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.phone,
+                                          color: Colors.blue),
+                                      const SizedBox(width: 5),
+                                      Text(contactNumber),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
             );
           },
         ),
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
           onPressed: () {
             showDialog(
               context: context,
@@ -161,7 +179,10 @@ class _HeadquartersPageState extends State<HeadquartersPage> {
               },
             );
           },
-          child: const Icon(Icons.add),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
         ),
       ),
     );
