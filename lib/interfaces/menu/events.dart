548642b291f56c3d83769c7bd8e7ded8fdd8f3b7
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-02-15 00:30:49 -0500

    Update events

--- lib/interfaces/menu/events.dart
+++ lib/interfaces/menu/events.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:refmp/controllers/exit.dart';
 import 'package:refmp/routes/menu.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+// import 'package:intl/intl.dart';
 
 class EventsPage extends StatefulWidget {
   const EventsPage({super.key, required this.title});
   final String title;
 
   @override
-  State<EventsPage> createState() => _EventsPage();
+  State<EventsPage> createState() => _EventsPageState();
 }
 
-class _EventsPage extends State<EventsPage> {
+class _EventsPageState extends State<EventsPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<Map<String, dynamic>>> _eventsByDay = {};
+  List<Map<String, dynamic>> _events = [];
+
+  Future<void> fetchEvents() async {
+    final response = await supabase
+        .from('events')
+        .select('*, sedes(name)')
+        .order('date', ascending: true)
+        .order('name', ascending: true);
+
+    Map<DateTime, List<Map<String, dynamic>>> eventsMap = {};
+    List<Map<String, dynamic>> eventsList = [];
+    for (var event in response) {
+      DateTime eventDate = DateTime.parse(event['date']);
+      DateTime normalizedDate =
+          DateTime(eventDate.year, eventDate.month, eventDate.day);
+      if (!eventsMap.containsKey(normalizedDate)) {
+        eventsMap[normalizedDate] = [];
+      }
+      eventsMap[normalizedDate]!.add(event);
+      eventsList.add(event);
+    }
+
+    setState(() {
+      _eventsByDay = eventsMap;
+      _events = eventsList
+          .where((e) => DateTime.parse(e['date']).month == _focusedDay.month)
+          .toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+    fetchEvents();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -36,10 +81,130 @@ class _EventsPage extends State<EventsPage> {
           ),
         ),
         drawer: Menu.buildDrawer(context),
-        body: const Center(
-          child: Text(
-            "Contenido de la página Eventos",
-            style: TextStyle(fontSize: 18),
+        body: RefreshIndicator(
+          onRefresh: fetchEvents,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TableCalendar(
+                  locale: 'es_ES',
+                  firstDay: DateTime.utc(2000, 1, 1),
+                  lastDay: DateTime.utc(2100, 12, 31),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                      _events = _eventsByDay[selectedDay] ?? [];
+                    });
+                  },
+                  onPageChanged: (focusedDay) {
+                    setState(() {
+                      _focusedDay = focusedDay;
+                      _events = _eventsByDay.entries
+                          .where((e) => e.key.month == focusedDay.month)
+                          .expand((e) => e.value)
+                          .toList();
+                    });
+                  },
+                  calendarFormat: CalendarFormat.month,
+                  headerStyle: const HeaderStyle(
+                      formatButtonVisible: false, titleCentered: true),
+                  calendarBuilders: CalendarBuilders(
+                    markerBuilder: (context, date, events) {
+                      if (_eventsByDay.containsKey(date)) {
+                        return Positioned(
+                          bottom: 5,
+                          child: Container(
+                            width: 8,
+                            height: 8,
+                            decoration: const BoxDecoration(
+                              color: Colors.green,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        );
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _events.length,
+                  itemBuilder: (context, index) {
+                    final event = _events[index];
+                    final sedeName =
+                        event['sedes']['name'] ?? 'Sede desconocida';
+                    return GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          builder: (context) {
+                            return SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  if (event['image'] != null)
+                                    Image.network(event['image'],
+                                        width: double.infinity,
+                                        fit: BoxFit.cover),
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            event['name'] ??
+                                                'Evento sin nombre',
+                                            style: const TextStyle(
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold)),
+                                        const SizedBox(height: 10),
+                                        Text("Fecha: ${event['date']}",
+                                            style:
+                                                const TextStyle(fontSize: 16)),
+                                        Text("Hora: ${event['time']}",
+                                            style:
+                                                const TextStyle(fontSize: 16)),
+                                        Text("Ubicación: ${event['location']}",
+                                            style:
+                                                const TextStyle(fontSize: 16)),
+                                        Text("Sede: $sedeName",
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold)),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.all(10),
+                        child: ListTile(
+                          title: Text(event['name'] ?? 'Evento sin nombre',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          subtitle: Text(
+                              "${event['date']} - ${event['location']} - Sede: $sedeName"),
+                          trailing: Text("${event['time']}",
+                              style: const TextStyle(fontSize: 16)),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
