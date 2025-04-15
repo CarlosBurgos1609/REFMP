deb07e1f0bd74682505497797530addbb3b9f834
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-04-15 00:23:09 -0500

    Update info

--- lib/interfaces/menu/info.dart
+++ lib/interfaces/menu/info.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:refmp/controllers/exit.dart';
+import 'package:refmp/routes/menu.dart';
+import 'package:refmp/theme/theme_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoPage extends StatefulWidget {
   const InfoPage({super.key, required this.title});
@@ -9,8 +14,120 @@ class InfoPage extends StatefulWidget {
 }
 
 class _InfoPageState extends State<InfoPage> {
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw 'No se pudo abrir $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final createdForImage = themeProvider.isDarkMode
+        ? "assets/images/appbar.png"
+        : "assets/images/logofn.png";
+
+    return WillPopScope(
+      onWillPop: () => showExitConfirmationDialog(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+          elevation: 0,
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(Icons.menu, color: Colors.white),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              );
+            },
+          ),
+        ),
+        drawer: Menu.buildDrawer(context),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Imagen "Creado para"
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(16)),
+                child: Image.asset(
+                  createdForImage,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Divider(
+                height: 40,
+                thickness: 2,
+                color: themeProvider.isDarkMode
+                    ? const Color.fromARGB(255, 34, 34, 34)
+                    : const Color.fromARGB(255, 197, 196, 196),
+              ),
+
+              // Autor
+              const Text(
+                'Autor de la aplicación',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: () =>
+                    _launchURL("https://carlosburgos1609.github.io/build/"),
+                child: Image.asset(
+                  'assets/images/logocab.png',
+                  height: 200,
+                  width: double.infinity,
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              Divider(
+                height: 40,
+                thickness: 2,
+                color: themeProvider.isDarkMode
+                    ? const Color.fromARGB(255, 34, 34, 34)
+                    : const Color.fromARGB(255, 197, 196, 196),
+              ),
+              const SizedBox(height: 10),
+
+              // Patrocinador
+              const Text(
+                'Patrocinado por:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: () => _launchURL("https://www.pasto.gov.co/"),
+                child:
+                    // Image.asset(
+                    //   'assets/images/alcaldia.png',
+                    //   height: 200,
+                    //   width: double.infinity,
+                    //   fit: BoxFit.contain,
+                    // ),
+                    ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(16)),
+                  child: Image.asset(
+                    themeProvider.isDarkMode
+                        ? "assets/images/alcaldia_dark.png"
+                        : "assets/images/alcaldia.png",
+                    height: 180,
+                    // width: double.infinity,
+                    // fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
