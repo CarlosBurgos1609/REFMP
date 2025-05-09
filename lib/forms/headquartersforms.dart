66dcc3d03437f3a87abc5fd5a6a168a5fd6bf665
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-05-09 00:39:33 -0500

    Update headquarters

--- lib/forms/headquartersforms.dart
+++ lib/forms/headquartersforms.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class HeadquartersForm extends StatefulWidget {
   const HeadquartersForm({super.key});
@@ -15,40 +18,73 @@ class _HeadquartersFormState extends State<HeadquartersForm> {
   final _descriptionController = TextEditingController();
   final _contactNumberController = TextEditingController();
   final _ubicationController = TextEditingController();
-  final _photoController = TextEditingController();
 
+  File? _selectedImage;
   bool _isLoading = false;
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<String?> _uploadImage(File imageFile) async {
+    final fileName = const Uuid().v4();
+    final storageResponse = await Supabase.instance.client.storage
+        .from('headquarters')
+        .upload('headquarters_images/$fileName.jpg', imageFile);
+
+    if (storageResponse.isNotEmpty) {
+      final publicUrl = Supabase.instance.client.storage
+          .from('headquarters')
+          .getPublicUrl('headquarters_images/$fileName.jpg');
+      return publicUrl;
+    }
+    return null;
+  }
 
   Future<void> _submitForm() async {
     if (_formKey.currentState!.validate()) {
+      if (_selectedImage == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Por favor selecciona una imagen.')),
+        );
+        return;
+      }
+
       setState(() {
         _isLoading = true;
       });
 
       try {
-        // Insertar datos en la tabla "sedes" de Supabase
+        final imageUrl = await _uploadImage(_selectedImage!);
+
+        if (imageUrl == null) {
+          throw Exception('No se pudo subir la imagen.');
+        }
+
         await Supabase.instance.client.from('sedes').insert({
           'name': _nameController.text,
           'address': _addressController.text,
           'description': _descriptionController.text,
           'contact_number': _contactNumberController.text,
           'ubication': _ubicationController.text,
-          'photo': _photoController.text,
+          'photo': imageUrl,
         });
 
-        // Mostrar mensaje de éxito
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-              content: Text(
-            'Sede creada exitosamente',
-            style: TextStyle(color: Colors.blue),
-          )),
+          const SnackBar(content: Text('Sede creada exitosamente')),
         );
 
-        // Limpiar el formulario después de enviar
         _formKey.currentState!.reset();
+        setState(() {
+          _selectedImage = null;
+        });
       } catch (e) {
-        // Mostrar mensaje de error
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Error al crear la sede: $e')),
         );
@@ -60,15 +96,42 @@ class _HeadquartersFormState extends State<HeadquartersForm> {
     }
   }
 
+  InputDecoration customInputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue, width: 2),
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _addressController.dispose();
+    _descriptionController.dispose();
+    _contactNumberController.dispose();
+    _ubicationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Crear Nueva Sede',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('Crear Nueva Sede',
+            style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_rounded, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -76,98 +139,56 @@ class _HeadquartersFormState extends State<HeadquartersForm> {
           key: _formKey,
           child: ListView(
             children: [
+              const SizedBox(height: 16),
               TextFormField(
                 controller: _nameController,
-                decoration: const InputDecoration(
-                    labelText: 'Nombre de la sede',
-                    fillColor: Colors.blue,
-                    labelStyle: TextStyle(color: Colors.blue),
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue))),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa el nombre de la sede';
-                  }
-                  return null;
-                },
+                decoration: customInputDecoration('Nombre de la sede'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Ingresa el nombre de la sede' : null,
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 10),
               TextFormField(
                 controller: _addressController,
-                decoration: const InputDecoration(
-                  labelText: 'Dirección',
-                  labelStyle: TextStyle(color: Colors.blue),
-                  hoverColor: Colors.blue,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa la dirección';
-                  }
-                  return null;
-                },
+                decoration: customInputDecoration('Dirección'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Ingresa la Dirección o lugar' : null,
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 10),
               TextFormField(
                 controller: _descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Descripción',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa una descripción';
-                  }
-                  return null;
-                },
+                decoration: customInputDecoration('Descripción'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Ingresa una breve descripción' : null,
+                maxLines: 3,
               ),
               const SizedBox(height: 16),
               TextFormField(
                 controller: _contactNumberController,
-                decoration: const InputDecoration(
-                  labelText: 'Número de contacto',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa un número de contacto';
-                  }
-                  return null;
-                },
+                decoration: customInputDecoration('Número de contacto'),
+                validator: (value) =>
+                    value!.isEmpty ? 'Ingresa el número de contacto' : null,
               ),
               const SizedBox(height: 16),
               TextFormField(
                 controller: _ubicationController,
-                decoration: const InputDecoration(
-                  labelText: 'Ubicación (URL del mapa)',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa la URL de la ubicación';
-                  }
-                  return null;
-                },
+                decoration: customInputDecoration('Ubicación (URL del mapa)'),
+                validator: (value) => value!.isEmpty
+                    ? 'Por favor, ingrese la url de la Ubicación'
+                    : null,
               ),
               const SizedBox(height: 16),
-              TextFormField(
-                controller: _photoController,
-                decoration: const InputDecoration(
-                  labelText: 'URL de la foto',
-                  border: OutlineInputBorder(),
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: _selectedImage != null
+                      ? Image.file(_selectedImage!, fit: BoxFit.cover)
+                      : const Center(child: Text('Selecciona una imagen')),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa la URL de la foto';
-                  }
-                  return null;
-                },
               ),
               const SizedBox(height: 24),
               _isLoading
@@ -188,16 +209,5 @@ class _HeadquartersFormState extends State<HeadquartersForm> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _addressController.dispose();
-    _descriptionController.dispose();
-    _contactNumberController.dispose();
-    _ubicationController.dispose();
-    _photoController.dispose();
-    super.dispose();
   }
 }
