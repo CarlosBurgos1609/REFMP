f6058a21a7778240aefc74c375ae9bdf50ade885
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-09-23 21:55:43 -0500

    update cup

--- lib/games/game/escenas/cup.dart
+++ lib/games/game/escenas/cup.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
