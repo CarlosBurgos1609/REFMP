dd760816fea729fbb9331aba83108a04a4cfb014
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2026-02-14 16:52:49 -0500

    Update icon

--- android/app/src/main/kotlin/com/music/refmp/MainActivity.kt
+++ android/app/src/main/kotlin/com/music/refmp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.music.refmp
 
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
@@ -10,16 +9,15 @@ class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Forzar visualización del splash screen en MIUI y otros fabricantes
+        // Configuración universal del splash screen para todos los fabricantes
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.statusBarColor = android.graphics.Color.TRANSPARENT
-        }
-        
-        // Ocultar barra de navegación durante el splash (opcional)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
+            
+            // Para MIUI (Xiaomi/Redmi) - forzar visualización
+            if (Build.MANUFACTURER.equals("Xiaomi", ignoreCase = true)) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            }
         }
     }
 }
