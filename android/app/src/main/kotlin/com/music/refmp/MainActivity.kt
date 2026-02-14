205e2d296f16842d16d93dbfd1eb429a13e7f0ad
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2026-02-14 16:33:08 -0500

    Update Pubspec in splash-icon

--- android/app/src/main/kotlin/com/music/refmp/MainActivity.kt
+++ android/app/src/main/kotlin/com/music/refmp/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.music.refmp
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Forzar visualización del splash screen en MIUI y otros fabricantes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+        }
+        
+        // Ocultar barra de navegación durante el splash (opcional)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+    }
+}
